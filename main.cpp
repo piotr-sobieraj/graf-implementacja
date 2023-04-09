4f8b13c42e00639bd9929a13167c27ac78f9f949
piotr sobieraj <96738453+piotr-sobieraj@users.noreply.github.com>
2023-04-09 20:14:23 +0000

    Printing and initialization by a table

--- main.cpp
+++ main.cpp
@@ -1,5 +1,56 @@
 #include <iostream>
+#include<cassert>
+using namespace std;
+
+class graph{
+    public: 
+        graph(const int &s){
+            this->size = s;
+            this -> vortex = new int*[this->size];
+            for (int i = 0; i < this->size; i++)
+                this->vortex[i] = new int[this->size];
+        }
+
+        void print(){
+            cout << endl;
+            for (int i = 0; i < this->size; i++){
+                
+            for (int j = 0; j < this->size; j++)
+                cout << vortex[i][j] << "\t";
+    
+                cout << endl;
+            }
+        }   
+
+        void set_vortex(const int *values){
+            for(int i = 0; i < this->size; i++)
+                for (int j = 0; j < this->size; j++)
+                    this->vortex[i][j] = values[i * this->size + j];
+        }
+        
+    int size;
+    int **vortex;
+};
+
+
+void run_tests(){
+    
+}
+
 
 int main() {
-  std::cout << "Hello World!\n";
+    graph g(3);
+
+
+    int values[] = {0, 1, 3, 
+                    2, 3, 4, 
+                    5, 6, 7};
+    g.set_vortex(values);
+
+    
+    
+    
+    g.print();
+    
+    
 }
