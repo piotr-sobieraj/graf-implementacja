19246d536fc03d2b8e6aa83b532e8ec74d045ce0
piotr sobieraj <96738453+piotr-sobieraj@users.noreply.github.com>
2023-04-09 20:26:13 +0000

    Czy skierowany?

--- .ccls-cache/@home@runner@graf-implementacja/main.cpp
+++ .ccls-cache/@home@runner@graf-implementacja/main.cpp
@@ -27,6 +27,15 @@ class graph{
                 for (int j = 0; j < this->size; j++)
                     this->vortex[i][j] = values[i * this->size + j];
         }
+
+        bool is_directed(){
+            for(int i = 0; i < this->size; i++)
+                for(int j = 0; j < this->size; j++)
+                    if(vortex[i][j] != vortex[j][i])
+                        return true;
+            
+            return false;
+        }
         
     int size;
     int **vortex;
@@ -34,11 +43,22 @@ class graph{
 
 
 void run_tests(){
-    
+    // Sprawdzenie rozmiaru 
+    graph g1(5);
+    assert(g1.size == 5);
+
+    // Graf nieskierowany
+    graph g2(3);
+    int values[] = {1, 1, 1, 
+                    1, 0, 1,
+                    1, 1, 0};
+    assert(g2.is_directed() == true);
 }
 
 
 int main() {
+    run_tests();
+    
     graph g(3);
 
 
